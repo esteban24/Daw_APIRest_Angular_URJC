238cd86beb2e1cd241d5eb819e83172065cdf9da
jsm5 <simoncete93@hotmail.com>
2015-04-27 12:40:27 +0200

    AÑADIDO JAVADOC

--- locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/App.java
+++ locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/App.java
@@ -1,5 +1,7 @@
 package es.sidelab.locosxelfutbol;
 
+/* JORGE SIMÓN MARTÍNEZ 	GIS + MAT */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
