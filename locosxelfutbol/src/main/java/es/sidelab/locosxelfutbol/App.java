e207129998d03a6c88f696004344413d6d799e46
Jorge93 <93jorge93@gmail.com>
2015-04-27 12:43:13 +0200

    Segundo aporte

--- locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/App.java
+++ locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/App.java
@@ -1,6 +1,10 @@
 package es.sidelab.locosxelfutbol;
 
-/* JORGE SIMÓN MARTÍNEZ 	GIS + MAT */
+/**
+ *  JORGE SIMÓN MARTÍNEZ 	GIS + MAT
+ *  JORGE ALCAIDE GARCÍA    GIS 
+ **/
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
