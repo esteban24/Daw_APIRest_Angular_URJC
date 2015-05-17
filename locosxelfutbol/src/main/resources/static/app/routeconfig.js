2e14411dd43484b41f8e6042a332903a4165b9dc
Jorge93 <93jorge93@gmail.com>
2015-05-17 19:46:19 +0200

    Añadido html equipo y arbitro

--- locosxelfutbol/src/main/resources/static/app/routeconfig.js
+++ locosxelfutbol/src/main/resources/static/app/routeconfig.js
@@ -7,6 +7,7 @@ function RouteConfig($routeProvider) {
 	$routeProvider.when('/', { templateUrl : "templates/home.html" });
 	$routeProvider.when('/home', { templateUrl : "templates/home.html" });
 	$routeProvider.when('/adminLogin', { templateUrl : "templates/adminLogin.html" });
-
+	$routeProvider.when('/team', { templateUrl : "templates/team.html" });
+	$routeProvider.when('/referee', { templateUrl : "templates/referee.html" });
 
 }
