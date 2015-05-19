54f12420771c7228a0b48fa06dff6a01ca01caeb
jsm5 <simoncete93@hotmail.com>
2015-05-20 01:05:13 +0200

    Modificada la clase CourtRepository y añadida CourtController

---
+++ locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/CourtController.java
@@ -0,0 +1,57 @@
+package es.sidelab.locosxelfutbol;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courtAdmin")
+public class CourtController {
+
+	@Autowired
+	private CourtRepository courtRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Court> getCourts() {
+		return courtRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Court getCourt(@PathVariable long id) {
+		return courtRepository.findOne(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Court> addCourt(@RequestBody Court court, HttpSession sesion){
+		//if((sesion!=null)&&((sesion.getAttribute("admin") != null)&&((Boolean)sesion.getAttribute("admin")))){
+			courtRepository.save(court);
+			return new ResponseEntity<>(court, HttpStatus.CREATED);
+		//}else{
+			//return null;
+		//}	
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
+	public void modificar(@PathVariable long id, @RequestBody Court court, HttpSession sesion){
+		//if((sesion!=null)&&((sesion.getAttribute("admin") != null)&&((Boolean)sesion.getAttribute("admin")))){
+			courtRepository.setName(id,court.getName());
+			courtRepository.setDescription(id,court.getDescription());
+		//}
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public void deleteReferee(@PathVariable long id, HttpSession sesion){
+		courtRepository.delete(id);
+	}
+
+}
+
--- locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/CourtRepository.java
+++ locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/CourtRepository.java
@@ -1,7 +1,21 @@
 package es.sidelab.locosxelfutbol;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CourtRepository extends JpaRepository<Court, Long> {
+Court findById(long id);
+	
+	@Modifying
+	@Transactional
+	@Query("update Court c set c.name = ?2 where c.id=?1")
+	void setName(long id, String name);
 
+	@Modifying
+	@Transactional
+	@Query("update Court c set c.description = ?2 where c.id=?1")
+	void setDescription(long id, String lastName);
 }
