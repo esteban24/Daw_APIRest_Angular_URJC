6865ce8d53e849aa76d930f026a2e5cb4738938e
Jorge93 <93jorge93@gmail.com>
2015-05-19 17:37:27 +0200

    Modificar arbitro añadido

---
+++ locosxelfutbol/src/main/java/es/sidelab/locosxelfutbol/RefereeRepository.java
@@ -0,0 +1,21 @@
+package es.sidelab.locosxelfutbol;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface RefereeRepository extends JpaRepository<Referee, Long> {
+	
+	@Modifying
+	@Transactional
+	@Query("update Referee r set r.name = ?2 where r.id=?1")
+	void setName(long id, String name);
+
+	@Modifying
+	@Transactional
+	@Query("update Referee r set r.lastName = ?2 where r.id=?1")
+	void setLastName(long id, String lastName);
+	
+}
