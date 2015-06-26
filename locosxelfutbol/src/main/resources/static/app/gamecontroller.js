6c8c3f2a409d916c04f8376484e8ae1f597b1d40
esteban <esteban@192.168.1.14>
2015-06-26 20:45:45 +0200

    Añadida la funcionalidad de obtener los datos del usuario para crear un partido en el torneo seleccionado. También se controla que el usuario introduzca valores erróneos y se ha creado el esqueleto de la función que llamará a la BBDD. Queda pendiente crear esta función que llamará al controlador footballManager.js que hará la consulta REST pertinente.

--- locosxelfutbol/src/main/resources/static/app/gamecontroller.js
+++ locosxelfutbol/src/main/resources/static/app/gamecontroller.js
@@ -11,9 +11,12 @@ function GameController(footballManager, $timeout, $location) {
 	vm.selectedTournamentId = {};
 	vm.games=[];
 	vm.game={};
-	vm.goalsLocal = {};
-	vm.goalsVisitant = {};
+	vm.goalsLocal = 0;
+	vm.goalsVisitant = 0;
+	vm.teamLocal = "";
+	vm.teamVisitant = "";
 	vm.error = false;
+	vm.preparedToSend = false;
 	vm.games= footballManager.getGames();
 	vm.tournaments= footballManager.getTournaments();
 	vm.valido = footballManager.getValido();
@@ -40,32 +43,101 @@ function GameController(footballManager, $timeout, $location) {
 		$location.path("/");
 	};
 	
+	//Función que se llama al pulsar en un torneo para obtener el id del torneo que el usuario ha seleccionado
 	vm.setTournamentId = function setTournamentId(id){
+		//Cada vez que pulsamos un torneo debemos inicializar las variables del controlador para que no haya problemas
+		vm.preparedToSend = false;
 		vm.error = false;
-		vm.goalsLocal = {};
-		vm.goalsVisitant = {};
+		vm.goalsLocal = 0;
+		vm.goalsVisitant = 0;
+		vm.teamLocal = "";
+		vm.teamVisitant = "";
+		//Asignamos el valor del id que nos pasa el usuario a la variable del controlador
 		vm.selectedTournamentId = id;
 	};
 	
-	vm.sendGoals = function(local, visitant){
-		var uno = document.getElementById("local"+local);
-		var dos = document.getElementById("visitant"+visitant);
-		if((uno.value < "0") || (dos.value < "0")){
+	//Función que recoge el id del torneo para obtener los valores de los inputs del usuario
+	vm.sendGoals = function(id){
+		//Obtenemos los valores de los inputs y los almacenamos en sus correspondientes variables
+		var goalsLocal = document.getElementById("local"+id).value;
+		var goalsVisitant = document.getElementById("visitant"+id).value;
+		var teamLocal = document.getElementById("teamLocal").value;
+		var teamVisitant = document.getElementById("teamVisitant").value;
+		//Si los goles son menor que 0 o los equipos son iguales mandamos un mensaje de error y establecemos
+		//la variable para preparar el envío a BBDD a false
+		if((goalsLocal < 0) || (goalsVisitant < 0) || (teamLocal == teamVisitant)){
 			vm.error = true;
+			vm.preparedToSend = false;
+		//En caso de que los parámetros sean correctos actualizamos las variables del controlador por  
+		//las introducidas por el usuario	
 		}else{
 			vm.error = false;
-			vm.setGoalsLocal(uno.value);
-			vm.setGoalsVisitant(dos.value);
+			vm.setGoalsLocal(goalsLocal);
+			vm.setGoalsVisitant(goalsVisitant);
+			vm.setTeamLocal(teamLocal);
+			vm.setTeamVisitant(teamVisitant);
+			//alerts para comprobar el valor de las variables (descomentar para probarlo en la aplicación visualmente)
 			alert(vm.goalsLocal);
 			alert(vm.goalsVisitant);
+			alert(vm.teamLocal);
+			alert(vm.teamVisitant);
+			vm.preparedToSend = true;
 		};
 	};
 	
+	//TODO esteban: método importante que creará en partido en el torneo correspondiente
+	vm.generateGame = function (idLocalTeam, idVisitantTeam, idTournament){
+		var pointsWin = 3;
+		var pointsDraw = 1;
+		var pointsLost = 0;
+		if(vm.preparedToSend){
+			/* Método que actualiza por una parte el equipo con sus estadísticas y crea el partido
+			 * 
+			 * Crear updateTeamFromTournament que dado un id de torneo, busca por id el equipo y modifica sus estadísticas
+			 * 
+			 * Crear addGame que añade un partido con los equipos seleccionados en el torneo con el id que le pasamos
+			 * 
+			 * Nota: Si es muy complicado, eliminar lo de tener más de un torneo (solo dejar el primero que teníamos)
+			 * 	     y luego en las cabeceras de estos métodos, únicamente quitar idTournmanetcy actualizar los equipos en general
+			 *       así como los partidos.
+			 *       
+			 *       Estos métodos tendrán que crearse en footballManager.js y desde alli llamar al servicio REST de UPDATE que hay
+			 *       que crearse en torneo (tanto addGame como updateTeamFromTournament).
+			 * 
+			if(vm.goalsLocal > vm.goalsVisitant){
+				footballManager.updateTeamFromTournament(idTournament, idLocalTeam, pointsWin, vm.goalsLocal, vm.goalsVisitant);
+				footballManager.updateTeamFromTournament(idTournament, idVisitantTeam, pointsLost, vm.goalsVisitant, vm.goalsLocal);
+				footballManager.addGame(idTournament, idLocalTeam, idVisistantTeam, vm.goalsLocal, vm.goalsVisitant);
+			}else if(vm.goalsLocal == vm.goalsVisitant){
+				footballManager.updateTeamFromTournament(idTournament, idLocalTeam, pointsDraw, vm.goalsLocal, vm.goalsVisitant);
+				footballManager.updateTeamFromTournament(idTournament, idVisitantTeam, pointsDraw, vm.goalsVisitant, vm.goalsLocal);
+				footballManager.addGame(idTournament, idLocalTeam, idVisistantTeam, vm.goalsLocal, vm.goalsVisitant);
+			}else if(vm.goalsLocal < goalsVisitant){
+				footballManager.updateTeamFromTournament(idTournament, idLocalTeam, pointsLost, vm.goalsLocal, vm.goalsVisitant);
+				footballManager.updateTeamFromTournament(idTournament, idVisitantTeam, pointsWin, vm.goalsVisitant, vm.goalsLocal);
+				footballManager.addGame(idTournament, idLocalTeam, idVisistantTeam, vm.goalsLocal, vm.goalsVisitant);
+			}else{
+				vm.error = true;
+			};*/
+		};
+	};
+	
+	//SETTERS ------------------------
+	
 	vm.setGoalsLocal = function(goals){
-		vm.goalsLocal = goals
+		vm.goalsLocal = goals;
 	};
 	
 	vm.setGoalsVisitant = function(goals){
-		vm.goalsVisitant = goals
-	}
+		vm.goalsVisitant = goals;
+	};
+	
+	vm.setTeamLocal = function(name){
+		vm.teamLocal = name;
+	};
+	
+	vm.setTeamVisitant = function(name){
+		vm.teamVisitant = name;
+	};
+	
 };
